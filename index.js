1902e804614d962684b4dbe92982422646faef2e
Jason Kennaly <jkennaly@gmail.com>
2022-01-11 14:17:47 -0800

    configured to trigger Sns Message for uncached lineups

--- index.js
+++ index.js
@@ -1,44 +1,71 @@
-const chromium = require('chrome-aws-lambda');
+const AWS = require("aws-sdk")
+const redis = require("redis");
+
+const client = redis.createClient({url: process.env.REDIS_URL});
 
 exports.handler = async (event, context, callback) => {
-    let lineupUrl = "https://festigram.app/site/festivals/Coachella/2020"
+    let lineupUrl = ""
     let responseCode = 200;
     console.log("request: " + JSON.stringify(event));
     
     if (event.queryStringParameters && event.queryStringParameters.lineupUrl) {
         lineupUrl = event.queryStringParameters.lineupUrl;
     }
-    
-    if (event.body) {
-        let body = JSON.parse(event.body)
-        if (body.festid) 
-            festid = body.festid;
+  	if(!lineupUrl) return callback('No lineupUrl query parameter')
+
+    const leKey = 'arach-lineup.' + lineupUrl
+  	let result;
+
+    try {
+    	//await chromium.font('./.fonts/NotoColorEmoji.ttf');
+        return client.connect()
+    	  	.then(() => client.get(leKey))
+    	  	.then(raw => JSON.parse(raw))
+			.catch(err => {
+				console.error('linedEvents Redis Error: ' + fgUrl)
+				console.error(err)
+			})
+			.then(data => {
+				//return real data should be 200
+				if (data && data.status !== 'pending') return callback(null, data)
+				//return placeholder-should be a 202 not 200
+				if (data) return callback(null, data)
+				console.log('Redis data not available, triggering collection')
+				const pendingObject = {
+					lineupUrl,
+					status: 'pending'
+					triggered: Date.now(),
+					eta: Date.now() + 120*1000
+				}
+				client.set(leKey, JSON.stringify(pendingObject), {
+					EX: 60 * 3
+				})
+				const awsRegion = "us-east-1";
+			    const snsTopic = 'arn:aws:sns:us-east-1:246401628237:UncheckedLineupUrls';
+			    const snsSubject = 'Arachnival Lineup';
+			    // Create publish parameters
+			    const message = JSON.stringify({lineupUrl})
+			    var params = {
+			      Message: message,
+			      Subject: snsSubject,
+			      TopicArn: snsTopic
+			    };
+			    var sns = new AWS.SNS({ region: awsRegion });
+			    return sns.publish(params).promise()
+			    	.then(() => {
+			    		return callback(null, pendingObject)
+			    	})
+			    	.catch(err => {
+			    		console.error('SNS publish Error')
+			    		console.error(err)
+			    		return callback(err)
+			    	})
+				//retunr placeholder-should be 202
+				
+		})
+    } catch (error) {
+        return callback(error);
+    } finally {
+    	client.quit()
     }
-  let result = null;
-  let browser = null;
-
-  try {
-  	//await chromium.font('./.fonts/NotoColorEmoji.ttf');
-    browser = await chromium.puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless,
-      ignoreHTTPSErrors: true,
-    });
-
-    let page = await browser.newPage();
-
-    await page.goto(lineupUrl);
-
-    result = await page.title();
-  } catch (error) {
-    return callback(error);
-  } finally {
-    if (browser !== null) {
-      await browser.close();
-    }
-  }
-  console.log('Spidey sensed', result)
-  return callback(null, result);
 };
